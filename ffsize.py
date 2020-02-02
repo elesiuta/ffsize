ba8a23bce442b1c4465a4b64b1f7c2c99ff4a6f9
eric <elesiuta@gmail.com>
2020-02-01 22:55:15 -0500

    improve status bar (both for getting file sizes and crc), find crc after file sizes, count read errors and record in csv if flag set

--- ffsize.py
+++ ffsize.py
@@ -6,39 +6,30 @@ import sys
 import zlib
 
 class StatusBar:
-    def __init__(self, title: str, total: int, display: bool) -> "StatusBar":
-        self.total = total
-        self.display = display
+    def __init__(self, title):
         terminal_width = shutil.get_terminal_size()[0]
-        if terminal_width < 16 or total <= 0:
-            self.display = False
-        if self.display:
-            self.bar_len = min(100, terminal_width - (7 + len(title)))
-            self.progress = 0
-            self.bar_progress = 0
-            sys.stdout.write(title + ": [" + "-"*self.bar_len + "]\b" + "\b"*self.bar_len)
-            sys.stdout.flush()
+        self.bar_len = min(100, terminal_width - (7 + len(title)))
+        self.progress = 0
+        self.bar_progress = 0
+        sys.stdout.write(title + ": [" + "-"*self.bar_len + "]\b" + "\b"*self.bar_len)
+        sys.stdout.flush()
 
-    def initTotal(self, total: int) -> None:
+    def initTotal(self, total):
         if total <= 0:
             self.endProgress()
-        elif self.progress == 0:
-            self.total = total
+        self.total = total
 
-    def update(self) -> None:
-        if self.display:
-            self.progress += 1
-            bar_progression = int(self.bar_len * self.progress // self.total) - self.bar_progress
-            if bar_progression != 0:
-                self.bar_progress += bar_progression
-                sys.stdout.write("#" * bar_progression)
-                sys.stdout.flush()
-
-    def endProgress(self) -> None:
-        if self.display:
-            sys.stdout.write("#" * (self.bar_len - self.bar_progress) + "]\n")
+    def update(self, progress = 1):
+        self.progress += progress
+        bar_progression = int(self.bar_len * self.progress // self.total) - self.bar_progress
+        if bar_progression != 0:
+            self.bar_progress += bar_progression
+            sys.stdout.write("#" * bar_progression)
             sys.stdout.flush()
-            self.display = False
+
+    def endProgress(self):
+        sys.stdout.write("#" * (self.bar_len - self.bar_progress) + "]\n")
+        sys.stdout.flush()
 
 def crc(file_name, prev = 0):
     with open(file_name,"rb") as f:
@@ -47,7 +38,7 @@ def crc(file_name, prev = 0):
     return prev
 
 def prettyCrc(prev):
-    return "%X"%(prev & 0xFFFFFFFF)
+    return str("%X"%(prev & 0xFFFFFFFF)).zfill(8)
 
 def writeCsv(file_name, data, enc = None, delimiter = ","):
     with open(file_name, "w", newline="", encoding=enc, errors="backslashreplace") as f:
@@ -72,13 +63,12 @@ def main():
     args = parser.parse_args()
     if os.path.isdir(args.path):
         # Initialize variables
-        if args.csv:
-            csvList = []
-        if args.crc:
-            totalCrc = 0
-            crc_status = StatusBar("Scanning files", 1, True)
-            crc_status.initTotal(sum(len(f) for r, d, f in os.walk(args.path, topdown=False)))
+        count_status = StatusBar("Calculating sizes")
+        count_status.initTotal(sum(len(f) for r, d, f in os.walk(args.path, topdown=False)))
+        csvList = []
+        csvDict = {}
         fileCount = 0
+        errorCount = 0
         dirCount = 0
         totalFileSize = 0
         totalFolderSize = 0
@@ -94,30 +84,57 @@ def main():
             # check each file
             for f in sorted(file_list):
                 fullPath = os.path.join(dir_path, f)
-                totalFileSize += os.path.getsize(fullPath)
-                # equivalent: totalSize += os.stat(dir_path + os.path.sep + f).st_size
-                if args.csv and args.crc:
-                    fileCrc = crc(fullPath)
-                    totalCrc = (totalCrc + fileCrc) % (0xFFFFFFFF + 1)
-                    csvList.append([f, os.path.getsize(fullPath), prettyCrc(fileCrc), "", "", ""])
-                elif args.csv:
-                    csvList.append([f, os.path.getsize(fullPath), "", "", "", ""])
-                elif args.crc:
-                    # match 7-zip CRC32
-                    totalCrc += crc(fullPath)
-                    totalCrc %= (0xFFFFFFFF + 1)
-                    # alternative: totalCrc = crc(fullPath, totalCrc)
-                    crc_status.update()
-        if args.crc:
-            crc_status.endProgress()
+                try:
+                    fileSize = os.path.getsize(fullPath)
+                    totalFileSize += fileSize
+                    # equivalent: totalFileSize += os.stat(dir_path + os.path.sep + f).st_size
+                except:
+                    fileSize = -1
+                    errorCount += 1
+                if args.csv:
+                    if args.crc:
+                        csvList.append(fullPath)
+                        csvDict[fullPath] = {"name": f, "size": fileSize}
+                    else:
+                        csvList.append([f, fileSize, "", "", "", ""])
+                count_status.update()
+        count_status.endProgress()
         # return results
         duSize = totalFileSize + totalFolderSize
         print("Total files: %s" %(fileCount))
         print("Total folders: %s" %(dirCount))
         print("Total file size: %s bytes" %("{:,}".format(totalFileSize)))
         print("Total file + folder size: %s bytes" %("{:,}".format(duSize)))
+        if errorCount > 0:
+            print("Error: %s files could not be accessed" %(errorCount))
+        # do crc after, time saved by doing everything together is negligible
         if args.crc:
-            print("CRC32 checksum for data is: %s" %(prettyCrc(totalCrc)))
+            crc_status = StatusBar("Calculating CRC")
+            crc_status.initTotal(totalFileSize)
+            totalCrc = 0
+            for dir_path, sub_dir_list, file_list in os.walk(rootDir):
+                for f in file_list:
+                    fullPath = os.path.join(dir_path, f)
+                    try:
+                        # match 7-zip CRC32 total for data
+                        fileCrc = crc(fullPath)
+                        totalCrc = (totalCrc + fileCrc) % (0xFFFFFFFF + 1)
+                        # alternative: totalCrc = crc(fullPath, totalCrc)
+                        if args.csv:
+                            csvDict[fullPath]["crc"] = prettyCrc(fileCrc)
+                            crc_status.update(csvDict[fullPath]["size"])
+                        else:
+                            crc_status.update(os.path.getsize(fullPath))
+                    except:
+                        if args.csv:
+                            csvDict[fullPath]["crc"] = -1
+            if args.csv:
+                for i in range(len(csvList)):
+                    if type(csvList[i]) == str:
+                        f = csvDict[csvList[i]]
+                        csvList[i] = [f["name"], f["size"], f["crc"], "", "", ""]
+            crc_status.endProgress()
+            print("CRC32 checksum for data: %s" %(prettyCrc(totalCrc)))
         if args.csv:
             writeCsv("filelist.csv", csvList, args.enc, args.delim)
     else:
